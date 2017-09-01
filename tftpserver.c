c887bdc0ca39940f09c9aa392bea5a5891661272
Andrew Vu <anv012@ucsd.edu>
2017-09-01 16:38:24 -0700

    Initialize Git Repo

---
+++ tftpserver.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+
+
+#define TIMEOUT 1000
+#define RETRYMAX 10
+#define PORT 6100
+
+main(int argc, char **argv)
+{
+	char buffer[512];
+
+	/*Create a socket */
+	int sock;
+	sock = socket(AF_INET,SOCK_DGRAM, 0);
+	int rec;
+
+	/*initialize socket structure*/
+	struct sockaddr_in server;
+	memset(&server, 0 , sizeof(server));
+    server.sin_family = AF_INET; //AF_INET signifies Internet IP Protocol
+    server.sin_addr.s_addr = htonl(INADDR_ANY); /*accepts from anny address*/
+    server.sin_port = htons(PORT);
+
+	if (sd < 0){
+		perror("Server: socket could not be created");
+		exit(0);
+	}
+
+	if (bind(sock, (struct sockaddr *) &server, sizeof(server)) < 0){
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+
+
+	for(;;){
+		rec = recv(sock, buf, sizeof(buf),0);
+		
+		if(rec >0) {
+			switch(opCode(buf)){
+				case '1':
+
+			}
+		}
+
+	}
+}
+
+char getOpcode(char )
